9d989157b64d7aef7289582b85c43ceed0185794
aman-user123 <aman.20240263@iit.ac.lk>
2026-04-18 13:03:25 +0530

    Part5 created few mappers and logger package

---
+++ src/main/java/com/smartcampus/exception/RoomDeleteException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.exception;
+
+/**
+ *
+ * @author seyedaman
+ */
+
+
+public class RoomDeleteException extends RuntimeException {
+    public RoomDeleteException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/smartcampus/exception/RoomNotFoundExceptionMapper.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.exception;
+
+/**
+ *
+ * @author seyedaman
+ */
+
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
+
+@Provider
+public class RoomNotFoundExceptionMapper implements ExceptionMapper<RoomNotFoundException> {
+
+    @Override
+    public Response toResponse(RoomNotFoundException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
+
+
